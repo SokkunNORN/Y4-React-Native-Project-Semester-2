98ea909ffdab2aa20d5381cd827214fc4548dcf0
sokkun <sokkun.norn2019@gmail.com>
2021-06-01 12:12:07 +0700

    add color and screen search

---
+++ screen/Search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import {
+    View,
+    Text
+} from 'react-native'
+
+const Search = () => {
+    return (
+        <>
+            <View><Text>This is search</Text></View>
+        </>
+    )
+}
--- screen/index.js
+++ screen/index.js
@@ -1,10 +1,12 @@
 
 import Chat from './Chat'
+import Search from './Search'
 import Discover from './Discover'
 import Setting from './Setting'
 
 export {
     Chat,
+    Search,
     Discover,
     Setting
 }
