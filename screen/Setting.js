9d5232b1b04b06f9f6fd1413e76220b29283a060
sokkun <sokkun.norn2019@gmail.com>
2021-06-02 14:39:20 +0700

    change backgroud color

--- screen/Setting.js
+++ screen/Setting.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import Header from '../components/header/Header'
+import {
+    Card,
+    Title,
+    Paragraph
+} from 'react-native-paper'
+import {
+    StyleSheet,
+    ScrollView
+} from 'react-native'
+import { SIZES } from '../constant'
 
 const Setting = () => {
     return (
@@ -8,8 +18,35 @@ const Setting = () => {
                 title="Settings"
                 icon="pencil-box-outline"
             />
+
+            <ScrollView>
+                <Card style={ styles.card }>
+                    <Card.Cover style={ styles.profileImg } source={ require('../asset/cover.jpeg') } />
+                    <Card.Content>
+                        <Title></Title>
+                        <Title>Kun Kun</Title>
+                    </Card.Content>
+                    <Card.Content>
+                        <Title></Title>
+                        <Title>Kun Kun</Title>
+                    </Card.Content>
+                    <Card.Content>
+                        <Title></Title>
+                        <Title>Kun Kun</Title>
+                    </Card.Content>
+                </Card>
+            </ScrollView>
         </>
     )
 }
 
 export default Setting
+
+const styles = StyleSheet.create({
+    card: {
+        margin: SIZES.base()
+    },
+    profileImg: {
+        height: SIZES.width - (2 * SIZES.base())
+    }
+})
