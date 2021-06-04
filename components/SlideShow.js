3b6e2d03f6465dd9c5852e9502329040322f7984
sokkun <sokkun.norn2019@gmail.com>
2021-06-04 15:28:05 +0700

    add bottom label

--- components/SlideShow.js
+++ components/SlideShow.js
@@ -7,13 +7,15 @@ import {
 } from 'react-native'
 import {
     Title,
-    Card
+    Paragraph,
+    Card,
+    Button
 } from 'react-native-paper'
 
 const SlideShow = () => {
     return (
         <>
-            <Card>
+            <Card style={ styles.card }>
                 <View style={ styles.viewFeature } >
                     <Text style={ styles.labelFeature }>FEATURED</Text>
                 </View>
@@ -21,6 +23,26 @@ const SlideShow = () => {
                     style={ styles.imgFeature }
                     source={ require('../asset/feature.jpeg') }
                 />
+                <View>
+                    <Card.Cover
+                        style={ styles.logoSlide }
+                        source={ require('../asset/news.png') }
+                    />
+                    <View style={ styles.title }>
+                        <Title style={ styles.name }>Reak Smey New Day</Title>
+                        <Paragraph style={ styles.joineder }>11628 Members</Paragraph>
+                    </View>
+                    <Button
+                        icon='check-circle'
+                        color={ COLORS.warning }
+                        style={ styles.joinedBtn }
+                        labelStyle={ styles.labelStyle }
+                        uppercase={ false }
+                        mode='outlined'
+                        onPress={() => {}}>
+                        Joined
+                    </Button>
+                </View>
             </Card>
         </>
     )
@@ -29,8 +51,14 @@ const SlideShow = () => {
 export default SlideShow
 
 const styles = StyleSheet.create({
+    card: {
+        backgroundColor: COLORS.primary,
+        borderRadius: SIZES.base(1.5)
+    },
     imgFeature: {
-        height: SIZES.width - (4 * SIZES.base())
+        height: SIZES.width - (4 * SIZES.base()),
+        borderTopStartRadius: SIZES.base(1.5),
+        borderTopRightRadius: SIZES.base(1.5)
     },
     viewFeature: {
         position: 'absolute', 
@@ -49,5 +77,38 @@ const styles = StyleSheet.create({
         paddingEnd: SIZES.base(),
         paddingTop: SIZES.base(1),
         paddingBottom: SIZES.base(1)
+    },
+    logoSlide: {
+        width: SIZES.base(7),
+        height: SIZES.base(7),
+        borderTopRightRadius: SIZES.base(1.5),
+        borderBottomLeftRadius: SIZES.base(1.5)
+    },
+    title: {
+        position: 'absolute',
+        marginStart: SIZES.base(8),
+        width: SIZES.width - (SIZES.base(28)),
+        height: SIZES.base(4)
+    },
+    name: {
+        color: COLORS.secondary1,
+        fontSize: SIZES.font(17)
+    },
+    joineder: {
+        color: COLORS.secondary1,
+        marginTop: -SIZES.base(1)
+    },
+    joinedBtn: {
+        position: 'absolute',
+        marginTop: SIZES.base(1),
+        borderRadius: SIZES.radius(4),
+        backgroundColor: COLORS.dark,
+        width: SIZES.base(13),
+        marginLeft: SIZES.width - (SIZES.base(19))
+    },
+    labelStyle: {
+        fontSize: SIZES.font(),
+        fontWeight: 'normal',
+        color: COLORS.secondary1
     }
 })
