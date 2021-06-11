18deccc1b12cc18639a5fdb9ba7e8fa424b5d87f
sokkun <sokkun.norn2019@gmail.com>
2021-06-11 18:05:13 +0700

    format code

--- components/SlideShow.js
+++ components/SlideShow.js
@@ -28,7 +28,6 @@ const SlideShow = ({
     }
 
     const onContentSizeChange = (contentWidth, _) => {
-
         setInterval(() => {
             if (contentWidth > SIZES.width) {
                 xPosition += SIZES.width
