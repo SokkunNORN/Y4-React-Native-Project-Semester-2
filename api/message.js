ac70a7ea75aa717e6deaade3dd201f56bcb8201d
sokkun <sokkun.norn2019@gmail.com>
2021-07-29 11:47:35 +0700

    update get message

--- api/message.js
+++ api/message.js
@@ -6,24 +6,20 @@ const docRef = firestore().collection("message")
 
 export const getMessages = async (uid, participant_id) => {
     let messages = []
-    const connectionsId = [uid, participant_id]
 
     await docRef
     .orderBy('created_at', 'asc')
     .get()
     .then(document => {
         document.forEach(element => {
-            if (
-                connectionsId.includes(element.data().uid) && 
-                connectionsId.includes(element.data().participant_id)
-            ) {
+            if (participant_id == element.data().participant_id) {
                 const message = {
                     created_at: dateFormat(element.data().created_at), 
                     uid: element.data().uid,
                     message: element.data().message,
                     participant_id: element.data().participant_id,
                     updated_at: dateFormat(element.data().updated_at),
-                    owner: participant_id === element.data().participant_id
+                    owner: uid === element.data().uid
                 }
                 messages.push(message)
             }
